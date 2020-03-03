da3e8a6474f3cb9a50a221827c19eebed7da854c
igrekus <upload234@mail.ru>
2020-03-03 18:50:23 +0300

    FIX: properly save s2p files

--- instrumentcontroller.py
+++ instrumentcontroller.py
@@ -170,8 +170,8 @@ class InstrumentController(QObject):
             pna.query('*OPC?')
             res = pna.query(f'CALC1:DATA:SNP? 2')
 
-            pna.send(f'CALC:DATA:SNP:PORTs:Save "1,2", "d:/ksa/psm/s{ucontrol:.01f}.s2p"')
-            pna.send(f'MMEM:STOR "d:/ksa/psm1/s{ucontrol:.01f}.s2p"')
+            pna.send(f'CALC:DATA:SNP:PORTs:Save "1,2", "d:/ksa/psm_analog_s2p/s{str(f"{ucontrol:.01f}").replace(".", "_")}.s2p"')
+            pna.send(f'MMEM:STOR "d:/ksa/psm_analog_ports2/s{str(f"{ucontrol:.01f}").replace(".", "_")}.s2p"')
 
             # with open(f's2p_{code}.s2p', mode='wt', encoding='utf-8') as f:
             #     f.write(res)
